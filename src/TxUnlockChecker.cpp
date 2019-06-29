725f01f8e5b6c009014bf37a584edd411c42019c
Sumo Gr <sumogr@mail.com>
2019-06-29 20:28:13 +0300

    Fix to compile for Sumo

--- src/TxUnlockChecker.cpp
+++ src/TxUnlockChecker.cpp
@@ -24,8 +24,8 @@ TxUnlockChecker::get_leeway(
         network_type net_type) const
 {
     return tx_block_height < get_v2height(net_type)
-            ? CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V1
-            : CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V2;
+            ? 240 // CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS
+            : 240; // same as above
 }
 
 bool
